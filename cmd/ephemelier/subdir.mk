76f0924cc4770603e2b339ec7f01967c199b4fd8
Markku Rossi <mtr@iki.fi>
2025-10-15 18:39:37 +0300

    Help Emacs compilation mode to follow directories.

--- cmd/ephemelier/subdir.mk
+++ cmd/ephemelier/subdir.mk
@@ -2,4 +2,5 @@ phony_targets = all clean debug
 
 .PHONY : $(phony_targets)
 $(phony_targets) :
+	@echo "make[1]: Entering directory '$(CURDIR)/$(TOP_SRCDIR)'"
 	@$(MAKE) subdir="$(CURDIR)" -C "$(TOP_SRCDIR)" $@
