1c91c6129f44b4d479750820430241fb43945bf9
Markku Rossi <mtr@iki.fi>
2025-11-18 18:38:29 +0200

    TLS connection as FD.

---
+++ kernel/fd_tls.go
@@ -0,0 +1,63 @@
+//
+// Copyright (c) 2025 Markku Rossi
+//
+// All rights reserved.
+//
+
+package kernel
+
+import (
+	"errors"
+	"io"
+
+	"github.com/markkurossi/ephemelier/crypto/tls"
+)
+
+// FDTLS implements TLS client and server FDs.
+type FDTLS struct {
+	conn *tls.Conn
+	// XXX key bundle with certificate and private key share.
+}
+
+// NewTLSFD creates a new TLS FD. The argument conn must be non-nil
+// for garbler and nil for evaluator.
+func NewTLSFD(conn *tls.Conn) *FD {
+	return NewFD(&FDTLS{
+		conn: conn,
+	})
+}
+
+// Close implements FD.Close.
+func (fd *FDTLS) Close() int {
+	if fd.conn == nil {
+		return 0
+	}
+	return mapError(fd.conn.Close())
+}
+
+// Read implements FD.Read.
+func (fd *FDTLS) Read(b []byte) int {
+	if fd.conn == nil {
+		return 0
+	}
+	n, err := fd.conn.Read(b)
+	if err != nil {
+		if errors.Is(err, io.EOF) {
+			return 0
+		}
+		return mapError(err)
+	}
+	return n
+}
+
+// Write implements FD.Write.
+func (fd *FDTLS) Write(b []byte) int {
+	if fd.conn == nil {
+		return len(b)
+	}
+	n, err := fd.conn.Write(b)
+	if err != nil {
+		return mapError(err)
+	}
+	return n
+}
