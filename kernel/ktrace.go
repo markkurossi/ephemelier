65e1f34972145cb42fa80484a0f62a838a01b4a2
Markku Rossi <mtr@iki.fi>
2025-12-18 07:08:53 +0100

    RUsage.

---
+++ kernel/ktrace.go
@@ -0,0 +1,163 @@
+//
+// Copyright (c) 2025 Markku Rossi
+//
+// All rights reserved.
+//
+
+package kernel
+
+import (
+	"encoding/hex"
+	"fmt"
+	"strings"
+
+	"github.com/markkurossi/ephemelier/crypto/tls"
+)
+
+func (proc *Process) ktracePrefix() {
+	if !proc.kern.params.Trace {
+		return
+	}
+	fmt.Printf("%7s %3d %-8s ", proc.pid, proc.pc, proc.prog.Name)
+}
+
+func (proc *Process) ktraceStats(rusage RUsage) {
+	if !proc.kern.params.Trace {
+		return
+	}
+	proc.ktracePrefix()
+	fmt.Printf("INFO %v", rusage)
+	fmt.Println()
+}
+
+func (proc *Process) ktraceHex(data []byte) {
+	if !proc.kern.params.TraceHex {
+		return
+	}
+	dump := hex.Dump(data)
+	lines := strings.Split(dump, "\n")
+
+	var separator = "    -------------------------------------------------------------------------"
+	fmt.Println()
+	fmt.Println(separator)
+
+	for idx, line := range lines {
+		var n string
+		for i := 1; i < len(line); i++ {
+			if line[i] == '0' && i+1 < len(line) && line[i+1] != ' ' {
+				n = n + " "
+			} else {
+				n += line[i:]
+				break
+			}
+		}
+		if idx+1 < len(lines) || len(n) > 0 {
+			fmt.Println(n)
+		}
+	}
+	fmt.Print(separator)
+}
+
+func (proc *Process) ktraceCall(sys *syscall) {
+	if !proc.kern.params.Trace {
+		return
+	}
+	const dataLimit = 16
+
+	proc.ktracePrefix()
+	fmt.Printf("CALL %s", sys.call)
+	switch sys.call {
+	case SysExit, SysClose, SysWait, SysCreatemsg, SysAccept,
+		SysTlsstatus, SysRecvfd:
+		fmt.Printf("(%d)", sys.arg0)
+
+	case SysSpawn, SysDial, SysListen:
+		fmt.Printf("(%q)", string(sys.argBuf[:sys.arg1]))
+
+	case SysRead, SysTlsserver, SysTlsclient, SysSendfd:
+		fmt.Printf("(%d, %d)", sys.arg0, sys.arg1)
+
+	case SysTlskex:
+		ht := tls.HandshakeType(sys.arg1)
+		fmt.Printf("(%d, ", sys.arg0)
+		if len(sys.argBuf) <= dataLimit {
+			fmt.Printf("%x, %s)", sys.argBuf, ht)
+		} else {
+			fmt.Printf("%x..., %d)", sys.argBuf[:dataLimit], ht)
+			proc.ktraceHex(sys.argBuf)
+		}
+
+	case SysWrite:
+		fmt.Printf("(%d, ", sys.arg0)
+		if sys.arg1 <= dataLimit {
+			fmt.Printf("%x, %d)", sys.argBuf[:sys.arg1], sys.arg1)
+		} else {
+			fmt.Printf("%x..., %d)", sys.argBuf[:dataLimit], sys.arg1)
+			proc.ktraceHex(sys.argBuf)
+		}
+
+	case SysYield:
+		fmt.Printf("(%d)", sys.pc)
+
+	case SysGetport:
+		if sys.arg1 > 0 {
+			fmt.Printf("(%s)", sys.argBuf[:sys.arg0])
+		} else {
+			fmt.Printf("(%s)", PID(sys.arg0))
+		}
+	}
+	fmt.Println()
+}
+
+func (proc *Process) ktraceRet(sys *syscall) {
+	if !proc.kern.params.Trace {
+		return
+	}
+	proc.ktracePrefix()
+	fmt.Printf("RET  %s ", sys.call)
+	if sys.arg0 < 0 {
+		fmt.Printf("%d %s", sys.arg0, Errno(-sys.arg0))
+	} else {
+		switch sys.call {
+		case SysSpawn:
+			fmt.Printf("%s", PID(sys.arg0))
+
+		case SysRead, SysCreatemsg, SysTlsserver:
+			fmt.Printf("%d", sys.arg0)
+			if len(sys.argBuf) > 0 {
+				proc.ktraceHex(sys.argBuf)
+			} else {
+				fmt.Printf(", nil")
+			}
+
+		case SysTlskex:
+			fmt.Printf("%d %s", sys.arg0, tls.HandshakeType(sys.arg0))
+			if len(sys.argBuf) > 0 {
+				fmt.Printf(", %d bytes", len(sys.argBuf))
+				proc.ktraceHex(sys.argBuf)
+			} else {
+				fmt.Printf(", nil")
+			}
+
+		default:
+			fmt.Printf("%d", sys.arg0)
+		}
+	}
+	fmt.Println()
+}
+
+func (proc *Process) ktraceExit() {
+	if !proc.kern.params.Trace {
+		return
+	}
+	proc.ktracePrefix()
+	fmt.Printf("EXIT %v\n", proc.exitVal)
+
+	proc.ktracePrefix()
+	fmt.Printf("RUSG utime=%v, stime=%v\n", proc.rusage.Utime,
+		proc.rusage.Stime)
+
+	proc.ktracePrefix()
+	fmt.Printf("RUSG g=%v, xor=%v, nxor=%v\n", proc.rusage.NumGates,
+		proc.rusage.NumXOR, proc.rusage.NumNonXOR)
+}
--- kernel/process.go
+++ kernel/process.go
@@ -9,7 +9,6 @@ package kernel
 import (
 	"bytes"
 	"crypto/rand"
-	"encoding/hex"
 	"errors"
 	"fmt"
 	"math/big"
@@ -18,7 +17,6 @@ import (
 	"sync"
 	"time"
 
-	"github.com/markkurossi/ephemelier/crypto/tls"
 	"github.com/markkurossi/ephemelier/eef"
 	"github.com/markkurossi/mpc"
 	"github.com/markkurossi/mpc/circuit"
@@ -47,6 +45,7 @@ type Process struct {
 	pc          uint16
 	fds         map[int32]*FD
 	exitVal     int32
+	rusage      RUsage
 }
 
 // ProcState defines process states.
@@ -79,18 +78,29 @@ func (st ProcState) String() string {
 	return fmt.Sprintf("{ProcState %d}", st)
 }
 
-// Stats provides process statistics.
-type Stats struct {
-	MPCD      time.Duration
+// RUsage provides process resource usage information.
+type RUsage struct {
+	Utime     time.Duration
+	Stime     time.Duration
 	NumGates  uint64
 	NumWires  uint64
 	NumXOR    uint64
 	NumNonXOR uint64
 }
 
-func (stats Stats) String() string {
-	return fmt.Sprintf("g=%v xor=%v nxor=%v d=%v",
-		stats.NumGates, stats.NumXOR, stats.NumNonXOR, stats.MPCD)
+// Add adds the argument RUsage data to this RUsage instance.
+func (rusage *RUsage) Add(o RUsage) {
+	rusage.Utime += o.Utime
+	rusage.Stime += o.Stime
+	rusage.NumGates += o.NumGates
+	rusage.NumWires += o.NumWires
+	rusage.NumXOR += o.NumXOR
+	rusage.NumNonXOR += o.NumNonXOR
+}
+
+func (rusage RUsage) String() string {
+	return fmt.Sprintf("g=%v xor=%v nxor=%v utime=%v",
+		rusage.NumGates, rusage.NumXOR, rusage.NumNonXOR, rusage.Utime)
 }
 
 func (proc *Process) diagnostics() bool {
@@ -277,11 +287,16 @@ run:
 
 		var outputs circuit.IO
 		var result []*big.Int
-		var stats Stats
+
+		now := time.Now()
+		proc.rusage.Stime += now.Sub(last)
+		last = now
+
+		var rusage RUsage
 
 		if state.Circ != nil {
 			// Pre-compiled circuit.
-			proc.circuitStats(&stats, state.Circ)
+			proc.circuitStats(&rusage, state.Circ)
 			if proc.diagnostics() {
 				state.Circ.PrintInputs(circuit.IDEvaluator, inputs)
 			}
@@ -325,10 +340,11 @@ run:
 		}
 
 		// Program fragment statistics.
-		now := time.Now()
-		stats.MPCD = now.Sub(last)
+		now = time.Now()
+		rusage.Utime += now.Sub(last)
 		last = now
-		proc.ktraceStats(stats)
+		proc.ktraceStats(rusage)
+		proc.rusage.Add(rusage)
 
 		// Decode syscall.
 		err = proc.decodeSyscall(sys, mpc.Results(result, outputs))
@@ -403,6 +419,9 @@ run:
 			return err
 		}
 	}
+
+	proc.ktraceExit()
+
 	return nil
 }
 
@@ -474,11 +493,16 @@ run:
 
 		var outputs circuit.IO
 		var result []*big.Int
-		var stats Stats
+
+		now := time.Now()
+		proc.rusage.Stime += now.Sub(last)
+		last = now
+
+		var rusage RUsage
 
 		if state.Circ != nil {
 			// Pre-compiled circuit.
-			proc.circuitStats(&stats, state.Circ)
+			proc.circuitStats(&rusage, state.Circ)
 			if proc.diagnostics() {
 				state.Circ.PrintInputs(circuit.IDGarbler, inputs)
 			}
@@ -523,10 +547,11 @@ run:
 		}
 
 		// Program fragment statistics.
-		now := time.Now()
-		stats.MPCD = now.Sub(last)
+		now = time.Now()
+		rusage.Utime += now.Sub(last)
 		last = now
-		proc.ktraceStats(stats)
+		proc.ktraceStats(rusage)
+		proc.rusage.Add(rusage)
 
 		// Decode syscall.
 		err := proc.decodeSyscall(sys, mpc.Results(result, outputs))
@@ -674,6 +699,8 @@ run:
 			return err
 		}
 	}
+
+	proc.ktraceExit()
 
 	return nil
 }
@@ -861,12 +888,11 @@ func (proc *Process) setPC(sys *syscall) (*eef.Circuit, error) {
 	return state, nil
 }
 
-func (proc *Process) circuitStats(stats *Stats, circ *circuit.Circuit) {
-	stats.NumGates = uint64(circ.NumGates)
-	stats.NumWires = uint64(circ.NumWires)
-	stats.NumXOR = circ.Stats[circuit.XOR] + circ.Stats[circuit.XNOR]
-	stats.NumNonXOR = circ.Stats[circuit.AND] + circ.Stats[circuit.OR] +
-		circ.Stats[circuit.INV]
+func (proc *Process) circuitStats(rusage *RUsage, circ *circuit.Circuit) {
+	rusage.NumGates = uint64(circ.NumGates)
+	rusage.NumWires = uint64(circ.NumWires)
+	rusage.NumXOR = circ.Stats.NumXOR()
+	rusage.NumNonXOR = circ.Stats.NumNonXOR()
 }
 
 type syscall struct {
@@ -970,136 +996,4 @@ func (proc *Process) debugf(format string, a ...interface{}) {
 	}
 	fmt.Printf("%s: ", proc.prog.Name)
 	fmt.Printf(format, a...)
-}
-
-func (proc *Process) ktracePrefix() {
-	if !proc.kern.params.Trace {
-		return
-	}
-	fmt.Printf("%7s %3d %-8s ", proc.pid, proc.pc, proc.prog.Name)
-}
-
-func (proc *Process) ktraceStats(stats Stats) {
-	if !proc.kern.params.Trace {
-		return
-	}
-	proc.ktracePrefix()
-	fmt.Printf("INFO %v", stats)
-	fmt.Println()
-}
-
-func (proc *Process) ktraceHex(data []byte) {
-	if !proc.kern.params.TraceHex {
-		return
-	}
-	dump := hex.Dump(data)
-	lines := strings.Split(dump, "\n")
-
-	var separator = "    -------------------------------------------------------------------------"
-	fmt.Println()
-	fmt.Println(separator)
-
-	for idx, line := range lines {
-		var n string
-		for i := 1; i < len(line); i++ {
-			if line[i] == '0' && i+1 < len(line) && line[i+1] != ' ' {
-				n = n + " "
-			} else {
-				n += line[i:]
-				break
-			}
-		}
-		if idx+1 < len(lines) || len(n) > 0 {
-			fmt.Println(n)
-		}
-	}
-	fmt.Print(separator)
-}
-
-func (proc *Process) ktraceCall(sys *syscall) {
-	if !proc.kern.params.Trace {
-		return
-	}
-	const dataLimit = 16
-
-	proc.ktracePrefix()
-	fmt.Printf("CALL %s", sys.call)
-	switch sys.call {
-	case SysExit, SysClose, SysWait, SysCreatemsg, SysAccept,
-		SysTlsstatus, SysRecvfd:
-		fmt.Printf("(%d)", sys.arg0)
-
-	case SysSpawn, SysDial, SysListen:
-		fmt.Printf("(%q)", string(sys.argBuf[:sys.arg1]))
-
-	case SysRead, SysTlsserver, SysTlsclient, SysSendfd:
-		fmt.Printf("(%d, %d)", sys.arg0, sys.arg1)
-
-	case SysTlskex:
-		ht := tls.HandshakeType(sys.arg1)
-		fmt.Printf("(%d, ", sys.arg0)
-		if len(sys.argBuf) <= dataLimit {
-			fmt.Printf("%x, %s)", sys.argBuf, ht)
-		} else {
-			fmt.Printf("%x..., %d)", sys.argBuf[:dataLimit], ht)
-			proc.ktraceHex(sys.argBuf)
-		}
-
-	case SysWrite:
-		fmt.Printf("(%d, ", sys.arg0)
-		if sys.arg1 <= dataLimit {
-			fmt.Printf("%x, %d)", sys.argBuf[:sys.arg1], sys.arg1)
-		} else {
-			fmt.Printf("%x..., %d)", sys.argBuf[:dataLimit], sys.arg1)
-			proc.ktraceHex(sys.argBuf)
-		}
-
-	case SysYield:
-		fmt.Printf("(%d)", sys.pc)
-
-	case SysGetport:
-		if sys.arg1 > 0 {
-			fmt.Printf("(%s)", sys.argBuf[:sys.arg0])
-		} else {
-			fmt.Printf("(%s)", PID(sys.arg0))
-		}
-	}
-	fmt.Println()
-}
-
-func (proc *Process) ktraceRet(sys *syscall) {
-	if !proc.kern.params.Trace {
-		return
-	}
-	proc.ktracePrefix()
-	fmt.Printf("RET  %s ", sys.call)
-	if sys.arg0 < 0 {
-		fmt.Printf("%d %s", sys.arg0, Errno(-sys.arg0))
-	} else {
-		switch sys.call {
-		case SysSpawn:
-			fmt.Printf("%s", PID(sys.arg0))
-
-		case SysRead, SysCreatemsg, SysTlsserver:
-			fmt.Printf("%d", sys.arg0)
-			if len(sys.argBuf) > 0 {
-				proc.ktraceHex(sys.argBuf)
-			} else {
-				fmt.Printf(", nil")
-			}
-
-		case SysTlskex:
-			fmt.Printf("%d %s", sys.arg0, tls.HandshakeType(sys.arg0))
-			if len(sys.argBuf) > 0 {
-				fmt.Printf(", %d bytes", len(sys.argBuf))
-				proc.ktraceHex(sys.argBuf)
-			} else {
-				fmt.Printf(", nil")
-			}
-
-		default:
-			fmt.Printf("%d", sys.arg0)
-		}
-	}
-	fmt.Println()
 }
