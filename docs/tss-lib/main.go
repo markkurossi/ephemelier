3659444cf585535fcaeab078371b102deb9f8560
Markku Rossi <mtr@iki.fi>
2026-01-19 11:24:25 +0100

    Cleaned up party ID creation.

--- docs/tss-lib/main.go
+++ docs/tss-lib/main.go
@@ -9,12 +9,13 @@ package main
 import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
-	"crypto/rand"
 	"encoding/binary"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
 	"math/big"
+	"os"
 	"sync"
 
 	"github.com/bnb-chain/tss-lib/ecdsa/keygen"
@@ -29,17 +30,46 @@ var (
 
 type Peer struct {
 	io      ot.IO
+	ctx     *tss.PeerContext
 	PartyID *tss.PartyID
 }
 
-func NewPeer(io ot.IO, id, moniker string) (*Peer, error) {
-	key, err := rand.Int(rand.Reader, big.NewInt(0).Lsh(big.NewInt(1), 128))
-	if err != nil {
-		return nil, err
+func makePartyID(id string) *tss.PartyID {
+	var keyData []byte
+	const moniker = "Ephemelier"
+
+	keyData = append(keyData, []byte(id)...)
+	keyData = append(keyData, []byte(moniker)...)
+
+	key := new(big.Int).SetBytes(keyData)
+
+	return tss.NewPartyID(id, moniker, key)
+}
+
+func NewPeer(io ot.IO, evaluator bool) (*Peer, error) {
+	ids := tss.SortPartyIDs(tss.UnSortedPartyIDs{
+		makePartyID("E"),
+		makePartyID("G"),
+	})
+
+	var this string
+	if evaluator {
+		this = "E"
+	} else {
+		this = "G"
 	}
+
+	var id *tss.PartyID
+	for _, i := range ids {
+		if i.Id == this {
+			id = i
+		}
+	}
+
 	return &Peer{
 		io:      io,
-		PartyID: tss.NewPartyID(id, moniker, key),
+		ctx:     tss.NewPeerContext(ids),
+		PartyID: id,
 	}, nil
 }
 
@@ -48,21 +78,16 @@ func (peer *Peer) debugf(format string, a ...interface{}) {
 	fmt.Printf("%v: %v", peer.PartyID.Id, msg)
 }
 
-func (peer *Peer) Keygen(ids tss.SortedPartyIDs) {
-	ctx := tss.NewPeerContext(ids)
-
-	partyIDMap := make(map[string]*tss.PartyID)
-	for _, id := range ids {
-		partyIDMap[id.Id] = id
-	}
-
-	// Keygen.
+// Keygen implements the threshold key generation.
+func (peer *Peer) Keygen() {
 	errCh := make(chan *tss.Error)
 	outCh := make(chan tss.Message)
 	endCh := make(chan keygen.LocalPartySaveData)
 	curve := elliptic.P256()
 
-	params := tss.NewParameters(curve, ctx, peer.PartyID, len(ids), len(ids))
+	n := len(peer.ctx.IDs())
+
+	params := tss.NewParameters(curve, peer.ctx, peer.PartyID, n, n)
 	party := keygen.NewLocalParty(params, outCh, endCh).(*keygen.LocalParty)
 
 	go func() {
@@ -121,12 +146,9 @@ func (peer *Peer) Keygen(ids tss.SortedPartyIDs) {
 			}
 			peer.debugf("compressed: %x\n", data)
 
-			data, err = json.Marshal(save)
+			err = peer.writeSaveData(save)
 			if err != nil {
-				log.Fatalf("json.Marshal: %v\n", err)
-			}
-			if false {
-				fmt.Printf("JSON: %v\n", string(data))
+				log.Fatal(err)
 			}
 
 			return
@@ -147,41 +169,58 @@ func (peer *Peer) Keygen(ids tss.SortedPartyIDs) {
 	}
 }
 
+func (peer *Peer) Sign() {
+}
+
 func main() {
-	fmt.Println("TSS Main")
+	flag.Parse()
+	if len(flag.Args()) != 1 {
+		log.Fatalf("usage: tss-lib keygen/sign\n")
+	}
 
 	tss.RegisterCurve("secp256r1", elliptic.P256())
 
 	pG, pE := p2p.Pipe()
 
-	e, err := NewPeer(pE, "E", "Ephemelier")
+	e, err := NewPeer(pE, true)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Printf("E: Id=%v, Key=%x\n", e.PartyID.Id, e.PartyID.Key)
 
-	g, err := NewPeer(pG, "G", "Ephemelier")
+	g, err := NewPeer(pG, false)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Printf("G: Id=%v, Key=%x\n", g.PartyID.Id, g.PartyID.Key)
 
-	ids := tss.SortPartyIDs(tss.UnSortedPartyIDs{
-		e.PartyID,
-		g.PartyID,
-	})
-
 	var wg sync.WaitGroup
 	wg.Add(2)
 
-	go func() {
-		defer wg.Done()
-		e.Keygen(ids)
-	}()
-	go func() {
-		defer wg.Done()
-		g.Keygen(ids)
-	}()
+	switch flag.Args()[0] {
+	case "keygen":
+		go func() {
+			defer wg.Done()
+			e.Keygen()
+		}()
+		go func() {
+			defer wg.Done()
+			g.Keygen()
+		}()
+
+	case "sign":
+		go func() {
+			defer wg.Done()
+			e.Sign()
+		}()
+		go func() {
+			defer wg.Done()
+			g.Sign()
+		}()
+
+	default:
+		log.Fatalf("invalid operation: %v\n", flag.Args()[0])
+	}
 
 	wg.Wait()
 }
@@ -227,4 +266,19 @@ func unmarshalMessage(data []byte) (tss.ParsedMessage, error) {
 		return nil, err
 	}
 	return tss.ParseWireMessage(msgData, &from, isBroadcast)
+}
+
+func (peer *Peer) writeSaveData(save keygen.LocalPartySaveData) error {
+	data, err := json.Marshal(save)
+	if err != nil {
+		return err
+	}
+	f, err := os.Create(fmt.Sprintf("peer-%v.share", peer.PartyID.Id))
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	_, err = f.Write(data)
+	return err
 }
