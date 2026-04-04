7a47590970274a2c8798f643178f8ea1292d016e
Markku Rossi <mtr@iki.fi>
2026-04-04 10:13:29 +0200

    Split syscall.c into separate files.

--- riscv/libc/src/syscall/write.c
+++ riscv/libc/src/syscall/write.c
@@ -8,23 +8,7 @@
 #include "unistd.h"
 
 ssize_t
-read(int fd, void *buf, size_t count)
-{
-  return syscall3(SYS_read, fd, (long) buf, count);
-}
-
-ssize_t
 write(int fd, const void *buf, size_t count)
 {
     return syscall3(SYS_write, fd, (long) buf, count);
-}
-
-void
-_exit(int status)
-{
-  syscall1(SYS_exit, status);
-
-  // Should never return.
-  while (1)
-    ;
 }
